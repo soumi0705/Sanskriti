4e9eb7cf8eb0b34bf21e56f77cb1bae560aa3f9d
kvnal <72142540+kvnal@users.noreply.github.com>
2021-03-15 14:16:43 +0530

    login-register page added

---
+++ log Final/js/main.js
@@ -0,0 +1,16 @@
+// register Js
+const forminfo=document.querySelector("#forminfo")
+const KYCLink ="" // local host address kyc link
+forminfo.addEventListener('submit',event=>{
+    event.preventDefault()
+    console.log(forminfo)
+    console.log(forminfo.elements)
+    if(forminfo.elements['pass'].value==forminfo.elements['pass2'].value){
+        console.log("ok")
+        window.location.href=KYCLink
+    }
+
+    else{
+        alert("confirm Password didn't Match!")
+    }
+})
