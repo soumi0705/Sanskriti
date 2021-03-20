5b47e11710f0149f679d6729d497cc022560cd90
kvnal <72142540+kvnal@users.noreply.github.com>
2021-03-20 23:50:57 +0530

    cryto added

--- dapp-ui/components/navbar.vue
+++ dapp-ui/components/navbar.vue
@@ -13,12 +13,22 @@
       </ul>
       
     </nav>
- 
+
+    <div class="crypto" @click="binanceLink()">
+      <span class="dollar" ref="refd" v-bind:style="{color:dcolor}">{{Dollar}}</span>
+      <span class="rupees" >{{Rupees}}</span>
+    </div>
+    <div class="search">
+      <input type="text" name="" id="" placeholder="Search">
+      <input type="submit" name="Search" id="">
+    </div>
     <property-form v-if="showModal">
       <h3 slot="header">Register Product</h3>
     </property-form>
   </div>
 </template>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>
+
 
 <script>
 import PropertyForm from "~/components/propertyForm.vue";
@@ -31,6 +41,9 @@ export default {
       showModal: false,
       address: null,
       Logo : Logo,
+      Dollar:"ETH $dollar",
+      Rupees:"₹Rupee",
+      dcolor:"#000000"
     };
   },
   components: {
@@ -38,10 +51,12 @@ export default {
   
   },
   methods: {
+  
     toggle() {
       const urlParams = new URLSearchParams(window.location.search);
       const verf = urlParams.get('verified');
       const a_no = urlParams.get('adhaarNo');
+      
       console.log(verf + " " +a_no);
       var regex = new RegExp(/[0-9]{4} [0-9]{4} [0-9]{4}/ );
       if(verf == "IDverified!" && regex.exec(a_no)){
@@ -51,6 +66,76 @@ export default {
         window.location.href = "http://localhost:5000/faceID";
       }
     },
+  
+  binanceLink(){
+    window.open('https://www.binance.com/en-IN/trade/ETH_USDT')
+  },
+  
+
+
+  //fetching
+  
+    _main() {
+    const _this=this;
+    const urlUSDINR = 'https://api.exchangeratesapi.io/latest?symbols=USD,INR&base=USD';
+    var xhttp = new XMLHttpRequest();        
+    xhttp.onreadystatechange = function () {
+      
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            const usd2inr = JSON.parse(this.responseText).rates.INR;
+            // _this.Dollar=usd2inr
+            console.log(usd2inr);
+             _this.crypto(usd2inr)
+
+        }
+    };
+    xhttp.open('GET', urlUSDINR, true);
+    xhttp.send();
+
+},
+async crypto(usd2inr) {
+    const url = 'https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT';
+    const _this=this;
+    var xhttp = new XMLHttpRequest();
+    var price =0
+    var priceN=0
+    var priceINR=0
+    setInterval(function(){
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.response);
+            // change color function
+            priceN = JSON.parse(this.response).price
+            if(priceN>price){
+                //green text 
+                _this.dcolor="#02C076"
+                // console.log('UP');
+            }
+            else{
+              _this.dcolor="#CF304A"
+                // console.log('down');
+                //red
+            }
+            price = priceN
+            let roundPrice= Math.round(price*100)/100 //usd
+            priceINR=Math.round(roundPrice*usd2inr)
+            _this.Rupees=`₹${priceINR}`
+            _this.Dollar=`1ETH $${roundPrice}`
+            //display price inr
+            // console.log(roundPrice);
+            // console.log('INR '+priceINR);
+            
+        }
+    };
+    xhttp.open('GET',url , true)
+    xhttp.send()},1000)
+}
+
+
+  },
+  created(){ 
+    this._main()
   }
 };
 </script>
--- dapp-ui/layouts/default.vue
+++ dapp-ui/layouts/default.vue
@@ -65,4 +65,65 @@ html {
   color: #fff;
   background-color: #35495e;
 }
+
+.crypto{
+     cursor: pointer;
+     user-select: none;
+     margin-top: 15vh;
+    margin-left: auto;
+    width: 20%;
+    display: block;
+        width: 15%;
+    display: block;
+    padding: 0 10px;
+    margin-right: 1%;
+    
+}
+ .crypto:hover {
+    background-color: #f0f0f0;
+}
+.crypto span{
+  
+  text-align:right;
+  padding-right:20px ;
+}
+
+.crypto .dollar{
+  font-size: 20px;
+  display: block;
+  
+}
+
+.crypto .rupees{
+  font-size: 15px;
+  color: #777777;
+  display: block;
+}
+.search {
+    width: 50%;
+    margin-left: auto;
+    margin-top: 5vh;
+    margin-right: auto;
+}
+
+.search input[type="text"]{
+  width: 80%;
+  background-color: #F3F4F6;
+  border: 2px #D1D5DB solid;
+  padding-left: 10px;
+  font-size: 14px;
+  border-radius: 40px;
+}
+
+.search input:focus { outline: none; }
+
+.search input[type="submit"]{
+  background-color: #111827;
+  font-size: 14px;
+  color: #fff;
+  font-size: 14px;
+  border-radius: 40px;
+
+
+}
 </style>
